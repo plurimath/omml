8173492234deab95e0db5c29beb2b984e920686b
Ronald Tse <ronald.tse@ribose.com>
2024-10-08 17:07:58 +0800

    feat: initial commit

---
+++ lib/omml.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'omml/version'
+
+module Oomml
+  class Error < StandardError; end
+
+  # Your code goes here...
+end
---
+++ spec/omml_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Oomml do
+  it 'has a version number' do
+    expect(Oomml::VERSION).not_to be nil
+  end
+
+  # it "does something useful" do
+  #   expect(false).to eq(true)
+  # end
+end
