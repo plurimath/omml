f8faa92b1423aef878edb58b92bed388d5738614
suleman-uzair <suleman@workytical.com>
2025-11-17 22:07:37 +0500

    Updated spec and omml.rb file

--- lib/omml.rb
+++ lib/omml.rb
@@ -2,13 +2,41 @@
 
 require 'lutaml/model'
 require_relative 'omml/version'
-# require_relative 'omml/ct_o_math_para'
 
 module Omml
   class Error < StandardError; end
   REGISTER_ID = :omml
 
-  def register_id
-    @register_id ||= Lutaml::Model::Register.new(REGISTER_ID).id
+  def self.register_id
+    return @register_id if @register_id
+
+    Lutaml::Model::GlobalRegister.register(
+      Lutaml::Model::Register.new(REGISTER_ID),
+    )
+    @register_id = REGISTER_ID
+  end
+
+  def self.parse(omml, **options)
+    klass = CTOMathPara
+
+    klass.from_xml(
+      omml,
+      register: extract_register_id(options),
+      **options,
+    )
+  end
+
+  def self.extract_register_id(options)
+    return register_id unless options.key?(:register)
+
+    register = options[:register]
+    case register
+    when Symbol, String then register
+    when Lutaml::Model::Register then register.id
+    else
+      raise ArgumentError, "Invalid register option: #{register.class}"
+    end
   end
 end
+
+require_relative 'omml/ct_o_math_para'
--- spec/omml_spec.rb
+++ spec/omml_spec.rb
@@ -1,11 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.describe Omml do
-  it 'has a version number' do
-    expect(Omml::VERSION).not_to be nil
-  end
+  describe "#parse" do
+    context "contains simple OMML value" do
+      let(:input) do
+        <<~OMML
+          <m:oMathPara>
+            <m:oMath>
+              <m:r>
+                <m:t>dy</m:t>
+              </m:r>
+            </m:oMath>
+          </m:oMathPara>
+        OMML
+      end
 
-  # it "does something useful" do
-  #   expect(false).to eq(true)
-  # end
+      it "parses correctly" do
+        expect(Omml.parse(input)).not_to be_nil
+      end
+    end
+  end
 end
